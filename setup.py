cfeb905fb7e4ec42bdd4dea19805852d435fe69b
grosenf1-nih <gabriel.rosenfeld@nih.gov>
2025-06-17 09:26:02 -0400

    Create setup.py for github based install

---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+# Load README for long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+# Parse requirements.txt for dependencies
+
+def parse_requirements(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+setuptools.setup(
+    name="PheTK",
+    version="0.1.0",
+    author="PheTK Team",
+    author_email="PheTK@mail.nih.gov",
+    description="Fast python library for Phenome Wide Association Studies (PheWAS)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/grosenf1-nih/PheTK",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=parse_requirements("requirements.txt"),
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'phewas=PheTK.PheWAS:main',
+        ],
+    },
+)
